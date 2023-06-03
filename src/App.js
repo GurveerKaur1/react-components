58f8af93d32f2254f7602a46d4c12c86d49a41d6
Gurveer <gurveer2202@gmail.com>
2023-06-03 01:17:29 -0500

    add changes

--- src/App.js
+++ src/App.js
@@ -13,28 +13,28 @@ function App() {
       description: 'Phall is basically a thick, tomato based curry made with Habanero or Scotch Bonnet peppers',
       sweetName: 'Phall Curry',
       price: '$50',
-      image: 'https://th.bing.com/th/id/R.8ac394618bb28c1ffbac80dc099826ac?rik=1TRUn2nXC6ztvg&riu=http%3a%2f%2fimages.unsplash.com%2fphoto-1541783245831-57d6fb0926d3%3fcrop%3dentropy%26cs%3dtinysrgb%26fit%3dmax%26fm%3djpg%26ixid%3dMnwxMjA3fDB8MXxzZWFyY2h8NHx8Y2hvY29sYXR8fDB8fHx8MTYyOTE5Njc1MQ%26ixlib%3drb-1.2.1%26q%3d80%26w%3d1080&ehk=7uZNJ39aeBNmR%2bh6BCU31S2Wrrf6M54KkbCvBCzAOhU%3d&risl=&pid=ImgRaw&r=0'
+      image: 'https://th.bing.com/th/id/R.ffe651004c6c5afe672b2fe79bdbcdc6?rik=ypbzBV%2fQcuI%2fQQ&pid=ImgRaw&r=0'
     },
     {
       id: 1002,
       sweetName: 'Samosa',
       price: '$25',
       description: 'A samosa is a fried pastry with a savory filling, including ingredients such as potatoes.',
-      image: 'http://127.0.0.1:5500/Tools%20and%20Automation/Responsive/assets/img/pexels-antony-trivet-13354489.jpg'
+      image: 'https://th.bing.com/th/id/R.e2f2c1206d0e38fea58bc6ebf3028425?rik=MSkKE6nRqSvqpQ&riu=http%3a%2f%2f4.bp.blogspot.com%2f-l429VyxMmc4%2fUypaEwqqPlI%2fAAAAAAAADG8%2fnAqNhweW-RQ%2fs1600%2fjonassamusa.jpg&ehk=SKJaydfXBEgvLnsZEPunhJB1O29ZQT3E83pbAHP0MTY%3d&risl=&pid=ImgRaw&r=0'
     },
     {
       id: 1003,
       sweetName: 'Noodles',
       price: '$20',
       description: 'Noodles are food made of dough and sometimes given various shapes.',
-      image: 'http://127.0.0.1:5500/Tools%20and%20Automation/Responsive/assets/img/pexels-engin-akyurt-2347383.jpg'
+      image: 'https://images.pexels.com/photos/2347383/pexels-photo-2347383.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     },
     {
       id: 1003,
       sweetName: 'Chiili Beef',
       price: '$60',
       description: 'Beef Chilli Dry is stir-fried beef rolled in Chinese sweet and spicy veggies sauce.',
-      image: 'http://127.0.0.1:5500/Tools%20and%20Automation/Responsive/assets/img/pexels-iconcom-800773.jpg'
+      image: 'https://images.pexels.com/photos/3535395/pexels-photo-3535395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
   ];
 
